9ff9c3a70a1e5f2f6653e486ade23dafc6a5ecd9
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-18 20:14:55 +0200

    use `run-applescript`

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
-var execFile = require('child_process').execFile;
+var runApplescript = require('run-applescript');
 
 module.exports = function (bundleId, cb) {
 	var script = 'tell application "Finder" to set app_path to application file id "' + bundleId + '" as string\ntell application "System Events" to get value of property list item "CFBundleName" of property list file (app_path & ":Contents:Info.plist")';
 
-	execFile('osascript', ['-e', script], function (err, stdout) {
+	runApplescript(script, function (err, res) {
 		if (err) {
-			return cb(err);
+			cb(err);
+			return;
 		}
 
-		cb(err, stdout.trim());
+		cb(null, res);
 	});
 };
