d6440e766ebed6ba10e8f84a062d4dee09f45f80
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-06 18:40:49 +0200

    Use a runtime check instead of using the `os` property

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 var runApplescript = require('run-applescript');
 
 module.exports = function (bundleId, cb) {
+	if (process.platform !== 'darwin') {
+		throw new Error('Only OS X systems are supported');
+	}
+
 	var script = 'tell application "Finder" to set app_path to application file id "' + bundleId + '" as string\ntell application "System Events" to get value of property list item "CFBundleName" of property list file (app_path & ":Contents:Info.plist")';
 
 	runApplescript(script, function (err, res) {
